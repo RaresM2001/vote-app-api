af91f6c5263c1a77e72fbbcf9e9ad1b2aad97c7a
Rares Modure <rares@Raress-MacBook-Pro.local>
2020-11-23 17:48:00 +0200

    Auth system done (with cookies)

---
+++ routers/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const authHandler = require('../handlers/auth.js')
+
+router.post('/admins', authHandler.createAdmin);
+router.post('/login', authHandler.login)
+router.post('/verify_token', authHandler.verifyToken)
+
+
+module.exports = router;
---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+const DAY = 1000 * 60 * 60 * 24; // 24 hours
+
+module.exports = {
+    DAY
+}
