76839aa43dc1a01b4d75aa29f02aaefdb63f2d0c
Rares Modure <m.rares956@yahoo.com>
2020-11-28 01:21:15 +0200

    Checking if member already exits before creating one.

--- routers/member.js
+++ routers/member.js
@@ -4,7 +4,8 @@ const handler = require('../handlers/member');
 
 router.post('/', handler.addMember);
 router.get('/', handler.getMembers);
-router.get('/:id', handler.getMember);
+router.get('/:id', handler.getMemberById);
+router.get('/by_email/:email', handler.getMemberByEmail);
 router.delete('/:id', handler.deleteMember);
 
 module.exports = router;
