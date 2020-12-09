db8b40bcb7547b8abd36c7ddcfae0966d5e351f5
Rares Modure <m.rares956@yahoo.com>
2020-12-09 19:10:41 +0200

    d

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://modure-rares-dev:<goldenLife7373>@voteapp.kux2v.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://modure-rares-dev:<${process.env.atlasPassword}>@voteapp.kux2v.mongodb.net/<${process.env.atlasDbName}>?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
