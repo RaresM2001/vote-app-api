47196d753dc4f7de85704f9387f568d8a9c2c0b6
Rares Modure <m.rares956@yahoo.com>
2020-12-09 18:27:47 +0200

    Connecting to atlas

--- database/index.js
+++ database/index.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/vote_application',
+mongoose.connect('mongodb+srv://modure-rares-dev:<goldenLife7373>@voteapp.kux2v.mongodb.net/<dbname>?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false
+    },
+    () => {
+        console.log('Connected to database!')
     }
 );
 
