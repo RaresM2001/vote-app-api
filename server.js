ab3f1a53350fe6dbbfd368a088dfb63370b9e4ca
Rares Modure <m.rares956@yahoo.com>
2020-12-10 02:44:35 +0200

    added serve-static

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const serveStatic = require('serve-static')
 
 
 require('./database');
