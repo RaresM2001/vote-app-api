7c4441730d8a3984a5824db7701d8b4d47036d02
Rares Modure <m.rares956@yahoo.com>
2021-03-05 18:41:25 +0200

    removed middleware from member count endpoint

--- routers/member.js
+++ routers/member.js
@@ -7,7 +7,7 @@ router.get('/code/:code', handler.findMemberByAccessCode);
 router.post('/', middleware.checkCookie, handler.addMember);
 router.post('/add_members', handler.addMembers)
 router.get('/:id', handler.getMembers);
-router.get('/count/:id', middleware.checkCookie, handler.getMemberCount);
+router.get('/count/:id', handler.getMemberCount);
 router.get('/member/:id', middleware.checkCookie, handler.getMemberById);
 router.get('/by_email/:email', middleware.checkCookie, handler.getMemberByEmail);
 router.delete('/:id', middleware.checkCookie, handler.deleteMember);
